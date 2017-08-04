8ca50417b8f93110ccd2dacdcbb3ac00ca80d897
nikita <mishinnikitam@gmail.com>
2017-08-04 10:23:25 +0300

    added authorization(with email confirmation)

---
+++ security/tokens.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils import six
+
+
+class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
+    @staticmethod
+    def makeHashValue(user, timestamp):
+        return (
+            six.text_type(user.pk) + six.text_type(timestamp) +
+            six.text_type(user.profile.email_confirmed)
+        )
+
+accountActivationToken = AccountActivationTokenGenerator()

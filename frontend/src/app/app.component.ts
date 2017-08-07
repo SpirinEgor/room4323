527d1892db1592760859bb074225cc6c117680f1
Egor Spirin <spirin.egor@mail.ru>
2017-08-08 02:18:53 +0300

    frontend init, header & footer

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'all-algorithms',
+  template: `<h1>There be modules</h1>`,
+})
+export class AppComponent  { name = 'Angular'; }

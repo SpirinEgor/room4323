cb1960948ed155efbeaebc50b0e218bf50cadc3a
Egor Spirin <spirin.egor@mail.ru>
2017-10-21 00:42:19 +0300

    offer http chenge to GET

--- frontend/src/app/offer/offer.component.ts
+++ frontend/src/app/offer/offer.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OfferAlgorithmService } from './offer.service';
-import { Router } from '@angular/router';
 
-import { error } from '../common/response';
-import { showInfoToast, showSuccToast, showErrorToast, serverNotRespone } from '../common/toast';
+import { showInfoToast } from '../common/toast';
 
 @Component({
     selector: 'offer',
@@ -17,8 +15,7 @@ export class OfferComponent implements OnInit {
     title = '';
     category = '';
 
-    constructor(private offerAlgorithmService: OfferAlgorithmService,
-                private router: Router) { }
+    constructor(private offerAlgorithmService: OfferAlgorithmService) { }
 
     ngOnInit() {
         this.getAllCategories();
@@ -72,21 +69,7 @@ export class OfferComponent implements OnInit {
         } else if (this.algorithm === '') {
             showInfoToast('You forget to write algorithm.');
         } else {
-            let result = this.offerAlgorithmService.offerAlgorithm(this.algorithm, this.title, this.category);
-            result.subscribe(
-                data => {
-                    let status = data.json().status.toString();
-                    if (status === error) {
-                        showErrorToast(data.json().message.toString());
-                    } else {
-                        showSuccToast('Your algorithm was successfuly offered. Wait for moderator to check it.');
-                        this.router.navigate(['']);
-                    }
-                },
-                err => {
-                    showErrorToast(serverNotRespone);
-                }
-            );
+            this.offerAlgorithmService.offerAlgorithm(this.algorithm, this.title, this.category);
         }
     }
 
--- frontend/src/app/offer/offer.service.ts
+++ frontend/src/app/offer/offer.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http';
+import { Http, URLSearchParams } from '@angular/http';
+import { Router } from '@angular/router';
+
 import * as Toast from '../common/toast';
 import * as Response from '../common/response';
 
@@ -8,7 +10,8 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class OfferAlgorithmService {
 
-    constructor(private $http: Http) { }
+    constructor(private $http: Http,
+                private router: Router) { }
 
     private handleError(error: any) {
         Toast.showErrorToast(Toast.serverNotRespone);
@@ -28,15 +31,22 @@ export class OfferAlgorithmService {
     }
 
     offerAlgorithm(newAlgorithm: string, title: string, category: string) {
-        const body = {
-            'algorithm': newAlgorithm,
-            'title': title,
-            'category': category
-        };
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        let options = new RequestOptions({ headers: headers, withCredentials: true });
-        return this.$http.post('http://api.room4323.study/api/article/create', body, options);
+        let params = new URLSearchParams();
+        params.append('algorithm', newAlgorithm);
+        params.append('title', title);
+        params.append('category', category);
+        return this.$http.get('http://api.room4323.study/api/article/create', {params: params})
+                    .toPromise()
+                    .then(response => {
+                        let status = response.json().status.toString();
+                        if (status === Response.error) {
+                            Toast.showErrorToast(response.json().message.toString());
+                        } else {
+                            Toast.showSuccToast('Your algorithm was successfuly offered. Wait for moderator to check it.');
+                            this.router.navigate(['']);
+                        }
+                    })
+                    .catch(this.handleError);
     }
 
 }
